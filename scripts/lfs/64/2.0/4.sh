28b683be45747784ca6db2f3988fe87f8b0d8d61
Chandrakant Singh <cks3383@gmail.com>
2015-07-12 06:37:45 +0530

    Added /etc/bash_completion script to take care of the have command not found issue

--- scripts/lfs/64/2.0/4.sh
+++ scripts/lfs/64/2.0/4.sh
@@ -252,6 +252,31 @@ echo 3.0.0-SYSTEMD > /etc/clfs-release
 
 fi
 
+if [ ! -f /etc/bash_completition ]
+then
+
+cat > /etc/bash_completion<<"EOF"
+# This function checks whether we have a given program on the system.
+#
+_have()
+{
+    # Completions for system administrator commands are installed as well in
+    # case completion is attempted via `sudo command ...'.
+    PATH=$PATH:/usr/sbin:/sbin:/usr/local/sbin type $1 &>/dev/null
+}
+
+# Backwards compatibility for compat completions that use have().
+# @deprecated should no longer be used; generally not needed with dynamically
+#             loaded completions, and _have is suitable for runtime use.
+have()
+{
+    unset -v have
+    _have $1 && have=yes
+}
+EOF
+
+fi
+
 clear
 
 echo "Done with building the base system. Now lets put some essential packages before rebooting"
