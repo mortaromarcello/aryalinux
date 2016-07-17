bc2ecc71243631afae536bb4b2ccd2ace01686a3
Chandrakant Singh <aryalinux11@gmail.com>
2016-07-17 21:21:42 +0530

    More fixes for virtualbox

--- blfs/virtualbox.sh
+++ blfs/virtualbox.sh
@@ -9,27 +9,39 @@ set +h
 
 cd $SOURCE_DIR
 
-wget -nc http://download.virtualbox.org/virtualbox/5.1.0/VirtualBox-5.1.0-108711-Linux_amd64.run
+if [ `uname -m` == "x86_64" ]
+then
+	wget -nc http://download.virtualbox.org/virtualbox/5.1.0/VirtualBox-5.1.0-108711-Linux_amd64.run
+else
+	wget -nc http://download.virtualbox.org/virtualbox/5.1.0/VirtualBox-5.1.0-108711-Linux_x86.run
+fi
 wget -nc https://www.kernel.org/pub/linux/kernel/v4.x/linux-4.5.tar.xz
 tar xf linux-4.5.tar.xz
-cp config-64 linux-4.5/.config
 if [ `uname -m` == "x86_64" ]
 then
 	wget -nc aryalinux.org/releases/2016.04/config-64
+	cp config-64 linux-4.5/.config
 else
 	wget -nc aryalinux.org/releases/2016.04/config-32
+	cp config-32 linux-4.5/.config
 fi
 pushd linux-4.5
 make oldconfig
 make prepare
 make scripts
 popd
-chmod a+x VirtualBox-5.1.0-108711-Linux_amd64.run
+if [ `uname -m` == "x86_64" ]
+then
+	chmod a+x VirtualBox-5.1.0-108711-Linux_amd64.run
+else
+	chmod a+x VirtualBox-5.1.0-108711-Linux_x86.run
+fi
 sudo KERN_DIR="`pwd`/linux-4.5" ./VirtualBox-5.1.0-108711-Linux_amd64.run
 
 sudo ln -svf /opt/VirtualBox/virtualbox.desktop /usr/share/applications/
 sudo update-desktop-database
-sudo update-mime-database /usr/share/mime
+su
+do update-mime-database /usr/share/mime
 
 cd $SOURCE_DIR
 rm -rf linux-4.5
