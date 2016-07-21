f7329ddad7def810be4f2e0b0b2e4f08ee236387
Chandrakant Singh <aryalinux11@gmail.com>
2016-07-22 00:04:44 +0530

    Added tlp script

---
+++ blfs/tlp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+set +h
+
+. /etc/alps/alps.conf
+
+cd $SOURCE_DIR
+
+wget https://github.com/linrunner/TLP/archive/master.zip -O tlp.zip
+unzip tlp.zip
+
+cd TLP-master
+
+make "-j`nproc`"
+sudo make install
+
+cd $SOURCE_DIR
+rm -rf TLP-master
+
+echo "tlp=>`date`" | sudo tee -a $INSTALLED_LIST
+
+
