af5f4fbf5d9fd39e6ea13f975553a9728116f9f0
Chandrakant Singh <aryalinux11@gmail.com>
2016-07-22 21:40:32 +0530

    Added openshot

---
+++ blfs/frei0r-plugins.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+set +h
+
+. /etc/alps/alps.conf
+
+#VER:frei0r.orig:_1.4
+
+#REQ:cairo
+
+URL=http://archive.ubuntu.com/ubuntu/pool/universe/f/frei0r/frei0r_1.4.orig.tar.gz
+
+cd $SOURCE_DIR
+
+wget -nc $URL
+TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
+DIRECTORY=`tar tf $TARBALL | cut -d/ -f1 | uniq`
+
+tar -xf $TARBALL
+cd $DIRECTORY
+
+./configure --prefix=/usr  &&
+make "-j`nproc`"
+sudo make install
+
+cd $SOURCE_DIR
+sudo rm -rf $DIRECTORY
+
+echo "frei0r-plugins=>`date`" | sudo tee -a $INSTALLED_LIST
