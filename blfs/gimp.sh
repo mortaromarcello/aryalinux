780326b480a2be62cb0447c61f93d7fbdb45458a
Chandrakant Singh <aryalinux11@gmail.com>
2016-09-05 05:16:30 +0530

    Separated gimp-help from gimp scripts

--- blfs/gimp.sh
+++ blfs/gimp.sh
@@ -37,7 +37,6 @@ cd $SOURCE_DIR
 URL=http://download.gimp.org/pub/gimp/v2.8/gimp-2.8.18.tar.bz2
 
 wget -nc http://download.gimp.org/pub/gimp/v2.8/gimp-2.8.18.tar.bz2 || wget -nc ftp://ftp.osuosl.org/pub/blfs/conglomeration/gimp/gimp-2.8.18.tar.bz2 || wget -nc http://ftp.lfs-matrix.net/pub/blfs/conglomeration/gimp/gimp-2.8.18.tar.bz2 || wget -nc http://ftp.osuosl.org/pub/blfs/conglomeration/gimp/gimp-2.8.18.tar.bz2 || wget -nc ftp://ftp.lfs-matrix.net/pub/blfs/conglomeration/gimp/gimp-2.8.18.tar.bz2 || wget -nc http://mirrors-usa.go-parts.com/blfs/conglomeration/gimp/gimp-2.8.18.tar.bz2
-wget -nc http://ftp.lfs-matrix.net/pub/blfs/conglomeration/gimp/gimp-help-2.8.2.tar.bz2 || wget -nc http://ftp.osuosl.org/pub/blfs/conglomeration/gimp/gimp-help-2.8.2.tar.bz2 || wget -nc ftp://ftp.lfs-matrix.net/pub/blfs/conglomeration/gimp/gimp-help-2.8.2.tar.bz2 || wget -nc http://mirrors-usa.go-parts.com/blfs/conglomeration/gimp/gimp-help-2.8.2.tar.bz2 || wget -nc ftp://ftp.osuosl.org/pub/blfs/conglomeration/gimp/gimp-help-2.8.2.tar.bz2 || wget -nc http://download.gimp.org/pub/gimp/help/gimp-help-2.8.2.tar.bz2
 
 TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
 DIRECTORY=`tar tf $TARBALL | cut -d/ -f1 | uniq | grep -v "^\.$"`
@@ -77,26 +76,6 @@ ENDOFROOTSCRIPT
 sudo chmod 755 rootscript.sh
 sudo ./rootscript.sh
 sudo rm rootscript.sh
-
-
-HELPTARBALL=`ls ../gimp-help*`
-HELPDIR=`tar tf $HELPTARBALL | cut -d/ -f1 | uniq`
-tar xf $HELPTARBALL
-cd $HELPDIR
-ALL_LINGUAS="" ./configure --prefix=/usr &&
-make
-
-
-
-sudo tee rootscript.sh << "ENDOFROOTSCRIPT"
-make install &&
-chown -R root:root /usr/share/gimp/2.0/help
-
-ENDOFROOTSCRIPT
-sudo chmod 755 rootscript.sh
-sudo ./rootscript.sh
-sudo rm rootscript.sh
-
 
 cd $SOURCE_DIR
 
