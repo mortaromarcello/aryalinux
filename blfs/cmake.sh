cb30e7ca11d86df928a56fa92f28b2be82680427
Chandrakant Singh <aryalinux11@gmail.com>
2016-09-05 09:00:38 +0530

    Fixed cmake

--- blfs/cmake.sh
+++ blfs/cmake.sh
@@ -5,19 +5,20 @@ set -e
 . /etc/alps/alps.conf
 . /var/lib/alps/functions
 
-#VER:cmake:3.6.0
+#VER:cmake:3.5.0
 
 #REC:curl
 #REC:libarchive
+#OPT:qt4
 #OPT:qt5
 #OPT:subversion
 
 
 cd $SOURCE_DIR
 
-URL=http://www.cmake.org/files/v3.6/cmake-3.6.0.tar.gz
+URL=http://www.cmake.org/files/v3.5/cmake-3.5.0.tar.gz
 
-wget -nc http://ftp.osuosl.org/pub/blfs/conglomeration/cmake/cmake-3.6.0.tar.gz || wget -nc http://ftp.lfs-matrix.net/pub/blfs/conglomeration/cmake/cmake-3.6.0.tar.gz || wget -nc ftp://ftp.lfs-matrix.net/pub/blfs/conglomeration/cmake/cmake-3.6.0.tar.gz || wget -nc ftp://ftp.osuosl.org/pub/blfs/conglomeration/cmake/cmake-3.6.0.tar.gz || wget -nc http://mirrors-usa.go-parts.com/blfs/conglomeration/cmake/cmake-3.6.0.tar.gz || wget -nc http://www.cmake.org/files/v3.6/cmake-3.6.0.tar.gz
+wget -nc http://mirrors-usa.go-parts.com/blfs/conglomeration/cmake/cmake-3.5.0.tar.gz || wget -nc http://ftp.osuosl.org/pub/blfs/conglomeration/cmake/cmake-3.5.0.tar.gz || wget -nc ftp://ftp.osuosl.org/pub/blfs/conglomeration/cmake/cmake-3.5.0.tar.gz || wget -nc http://www.cmake.org/files/v3.5/cmake-3.5.0.tar.gz || wget -nc ftp://ftp.lfs-matrix.net/pub/blfs/conglomeration/cmake/cmake-3.5.0.tar.gz || wget -nc http://ftp.lfs-matrix.net/pub/blfs/conglomeration/cmake/cmake-3.5.0.tar.gz
 
 TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
 DIRECTORY=`tar tf $TARBALL | cut -d/ -f1 | uniq | grep -v "^\.$"`
@@ -31,7 +32,9 @@ whoami > /tmp/currentuser
             --system-libs       \
             --mandir=/share/man \
             --no-system-jsoncpp \
-            --docdir=/share/doc/cmake-3.6.0 &&
+            --docdir=/share/doc/cmake-3.5.0 \
+            --no-system-curl    \
+            --no-system-libarchive &&
 make "-j`nproc`"
 
 
