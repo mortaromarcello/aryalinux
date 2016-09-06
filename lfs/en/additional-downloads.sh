5990665dba3dc7e6622c8642edf9c8499b23d317
Chandrakant Singh <chandrakant.singh@kohls.com>
2016-09-06 17:05:31 +0530

    Fixed squashfs download

--- lfs/en/additional-downloads.sh
+++ lfs/en/additional-downloads.sh
@@ -42,11 +42,10 @@ wget -nc http://aryalinux.org/releases/2016.08/aufs4-mmap.patch
 wget -nc http://aryalinux.org/releases/2016.08/aufs4-standalone.patch
 wget -nc http://aryalinux.org/releases/2016.08/0.21-nvme_ioctl.h.patch
 wget -nc http://aryalinux.org/releases/2016.08/alps-scripts-2016.08.tar.gz
-rm -rf cdrtools-3.01.tar.bz2
-wget -nc https://sourceforge.net/projects/cdrtools/files/cdrtools-3.01.tar.bz2/download -O cdrtools-3.01.tar.bz2
+wget -c https://sourceforge.net/projects/cdrtools/files/cdrtools-3.01.tar.bz2/download -O cdrtools-3.01.tar.bz2
 wget -nc https://launchpad.net/ubuntu/+archive/primary/+files/cdrkit_1.1.11.orig.tar.gz
 wget -nc http://www.cmake.org/files/v3.5/cmake-3.5.0.tar.gz
-wget -nv http://pkgs.fedoraproject.org/repo/pkgs/squashfs-tools/squashfs4.3.tar.gz/370d0470f3c823bf408a3b7a1f145746/squashfs4.3.tar.gz
+wget -nc http://pkgs.fedoraproject.org/repo/pkgs/squashfs-tools/squashfs4.3.tar.gz/370d0470f3c823bf408a3b7a1f145746/squashfs4.3.tar.gz
 wget -nc aryalinux.org/releases/2016.08/bootx64.efi
 
 popd
