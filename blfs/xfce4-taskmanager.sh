47a29e6a8205620b5bd0d59813f9891f54182d56
Chandrakant Singh <aryalinux11@gmail.com>
2016-09-13 10:36:20 +0530

    Added xfce4-taskmanager

---
+++ blfs/xfce4-taskmanager.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+set +h
+
+. /etc/alps/alps.conf
+
+URL="http://archive.xfce.org/src/apps/xfce4-taskmanager/1.0/xfce4-taskmanager-1.0.0.tar.bz2"
+
+cd $SOURCE_DIR
+wget -nc $URL
+
+TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
+DIRECTORY=`tar -tf $TARBALL | cut -d/ -f1 | uniq`
+
+tar xf $TARBALL
+cd $DIRECTORY
+
+./configure --prefix=/usr &&
+make "-j`nproc`"
+sudo make install
+
+cd $SOURCE_DIR
+
+rm -r $DIRECTORY
+echo "xfce4-taskmanager=>`date`" | sudo tee -a $INSTALLED_LIST
