9798a9c54de01b2cf5f24ac4cbf830d98935d70f
Chandrakant <aryalinux11@gmail.com>
2016-09-15 11:20:49 +0530

    Added gstreamer-0.10.x scripts and exaile

---
+++ blfs/exaile.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+set +h
+
+. /etc/alps/alps.conf
+
+PACKAGE_NAME="exaile"
+
+#REQ:gstreamer-0.10
+#REQ:gstreamer-0.10-plugins-base
+#REQ:gstreamer-0.10-plugins-good
+#REQ:gstreamer-0.10-plugins-bad
+#REQ:gstreamer-0.10-plugins-ugly
+#REQ:gstreamer-0.10-ffmpeg
+#REQ:mutagen
+#REQ:gstreamer-0.10-python
+
+cd $SOURCE_DIR
+URL="http://archive.ubuntu.com/ubuntu/pool/universe/e/exaile/exaile_3.4.5.orig.tar.gz"
+wget -nc $URL
+
+TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
+DIRECTORY=`tar tf $TARBALL | cut -d/ -f1 | uniq`
+
+tar xf $TARBALL
+cd $DIRECTORY
+
+./configure --prefix=/usr
+make
+sudo make install
+
+cd $SOURCE_DIR
+sudo rm -rf $DIRECTORY
+
+echo "$PACKAGE_NAME=>`date`" | sudo tee -a $INSTALLED_LIST
