47ba51678a338abc0251d5d35c7f6e81743928ab
Chandrakant Singh <aryalinux11@gmail.com>
2016-09-22 14:50:41 +0530

    Added hardinfo

---
+++ blfs/hardinfo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+set +h
+
+. /etc/alps/alps.conf
+
+#VER:hardinfo:0.5.1
+
+URL=http://archive.ubuntu.com/ubuntu/pool/universe/h/hardinfo/hardinfo_0.5.1.orig.tar.gz
+
+cd $SOURCE_DIR
+
+wget -nc $URL
+TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
+DIRECTORY=`tar tf $TARBALL | cut -d/ -f1 | uniq`
+
+tar -xf $TARBALL
+cd $DIRECTORY
+
+./configure --prefix=/usr --build=aryalinux &&
+make "-j`nproc`"
+sudo make install
+
+cd $SOURCE_DIR
+sudo rm -rf $DIRECTORY
+
+echo "hardinfo=>`date`" | sudo tee -a $INSTALLED_LIST
