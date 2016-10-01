b39de1e66852ba4c03a54e65e9571f39d3f62c16
Chandrakant Singh <cks3383@gmail.com>
2016-10-01 20:27:01 +0530

    Fixed patch download location for xorg-server

--- blfs/xorg-server.sh
+++ blfs/xorg-server.sh
@@ -32,6 +32,7 @@ cd $SOURCE_DIR
 
 URL=http://ftp.x.org/pub/individual/xserver/xorg-server-1.18.4.tar.bz2
 
+wget -nc http://www.linuxfromscratch.org/patches/downloads/xorg-server/xorg-server-1.18.4-add_prime_support-1.patch
 wget -nc http://www.linuxfromscratch.org/patches/blfs/7.10/xorg-server-1.18.4-add_prime_support-1.patch || wget -nc http://www.linuxfromscratch.org/patches/downloads/xorg-server/xorg-server-1.18.4-add_prime_support-1.patch
 wget -nc http://ftp.x.org/pub/individual/xserver/xorg-server-1.18.4.tar.bz2 || wget -nc ftp://ftp.lfs-matrix.net/pub/blfs/conglomeration/Xorg/xorg-server-1.18.4.tar.bz2 || wget -nc http://ftp.lfs-matrix.net/pub/blfs/conglomeration/Xorg/xorg-server-1.18.4.tar.bz2 || wget -nc ftp://ftp.x.org/pub/individual/xserver/xorg-server-1.18.4.tar.bz2 || wget -nc http://ftp.osuosl.org/pub/blfs/conglomeration/Xorg/xorg-server-1.18.4.tar.bz2 || wget -nc http://mirrors-usa.go-parts.com/blfs/conglomeration/Xorg/xorg-server-1.18.4.tar.bz2 || wget -nc ftp://ftp.osuosl.org/pub/blfs/conglomeration/Xorg/xorg-server-1.18.4.tar.bz2
 
