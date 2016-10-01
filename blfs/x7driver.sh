18c1f6cac9a143bdd473749514aaea721cdd5650
Chandrakant Singh <cks3383@gmail.com>
2016-10-01 20:49:13 +0530

    Fixed an issue with x7driver.sh

--- blfs/x7driver.sh
+++ blfs/x7driver.sh
@@ -80,7 +80,7 @@ cd $DIRECTORY
             --with-udev-dir=/lib/udev &&
 make
 
-ln -sfv ld-2.23.so.dbg /lib/ld-linux-x86-64.so.2
+# ln -sfv ld-2.23.so.dbg /lib/ld-linux-x86-64.so.2
 
 
 sudo tee rootscript.sh << "ENDOFROOTSCRIPT"
