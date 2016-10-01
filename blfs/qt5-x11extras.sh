960a6828554a2002445a4201a8afd3831c6a0c4f
Chandrakant Singh <cks3383@gmail.com>
2016-10-02 01:07:17 +0530

    Added qt5-x11extras

---
+++ blfs/qt5-x11extras.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e
+
+. /etc/alps/alps.conf
+. /var/lib/alps/functions
+
+#VER:qt5-x11extras:5.6.1
+
+#REQ:qt5
+
+
+cd $SOURCE_DIR
+
+URL=http://archive.ubuntu.com/ubuntu/pool/universe/q/qtx11extras-opensource-src/qtx11extras-opensource-src_5.6.1.orig.tar.xz
+
+wget -nc $URL
+
+TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
+DIRECTORY=`tar tf $TARBALL | cut -d/ -f1 | uniq | grep -v "^\.$"`
+
+tar xf $TARBALL
+cd $DIRECTORY
+
+whoami > /tmp/currentuser
+
+mkdir build &&
+cd    build &&
+cmake -DCMAKE_INSTALL_PREFIX=/usr .. &&
+make "-j`nproc`"
+
+
+
+sudo tee rootscript.sh << "ENDOFROOTSCRIPT"
+make install
+
+ENDOFROOTSCRIPT
+sudo chmod 755 rootscript.sh
+sudo ./rootscript.sh
+sudo rm rootscript.sh
+
+
+cd $SOURCE_DIR
+
+sudo rm -rf $DIRECTORY
+echo "qt5-x11extras=>`date`" | sudo tee -a $INSTALLED_LIST
+
