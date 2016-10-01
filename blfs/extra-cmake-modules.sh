b6bcc8d8b5b8816b8dcf1ddcc42d9735091d8051
Chandrakant Singh <cks3383@gmail.com>
2016-10-02 01:04:37 +0530

    bumped ecm version

--- blfs/extra-cmake-modules.sh
+++ blfs/extra-cmake-modules.sh
@@ -5,16 +5,16 @@ set -e
 . /etc/alps/alps.conf
 . /var/lib/alps/functions
 
-#VER:extra-cmake-modules:5.23.0
+#VER:extra-cmake-modules:5.25.0
 
 #REQ:cmake
 
 
 cd $SOURCE_DIR
 
-URL=http://download.kde.org/stable/frameworks/5.23/extra-cmake-modules-5.23.0.tar.xz
+URL=http://download.kde.org/stable/frameworks/5.25/extra-cmake-modules-5.25.0.tar.xz
 
-wget -nc http://ftp.lfs-matrix.net/pub/blfs/conglomeration/extra-cmake-modules/extra-cmake-modules-5.23.0.tar.xz || wget -nc ftp://ftp.lfs-matrix.net/pub/blfs/conglomeration/extra-cmake-modules/extra-cmake-modules-5.23.0.tar.xz || wget -nc http://mirrors-usa.go-parts.com/blfs/conglomeration/extra-cmake-modules/extra-cmake-modules-5.23.0.tar.xz || wget -nc ftp://ftp.osuosl.org/pub/blfs/conglomeration/extra-cmake-modules/extra-cmake-modules-5.23.0.tar.xz || wget -nc http://download.kde.org/stable/frameworks/5.23/extra-cmake-modules-5.23.0.tar.xz || wget -nc http://ftp.osuosl.org/pub/blfs/conglomeration/extra-cmake-modules/extra-cmake-modules-5.23.0.tar.xz
+wget -nc $URL
 
 TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
 DIRECTORY=`tar tf $TARBALL | cut -d/ -f1 | uniq | grep -v "^\.$"`
