888888a4937c25ba5d7900085620678834875391
Chandrakant Singh <aryalinux11@gmail.com>
2016-10-08 08:36:03 +0530

    Fixed simple-screen-recorder

--- blfs/simple-screen-recorder.sh
+++ blfs/simple-screen-recorder.sh
@@ -20,12 +20,12 @@ tar -xf $TARBALL
 
 cd $DIRECTORY
 
-export QT5PREFIX="/opt/QT5"
+export QT5PREFIX="/opt/qt5"
 export QT5BINDIR="$QT5PREFIX/bin"
 export QT5DIR="$QT5PREFIX"
 export QTDIR="$QT5PREFIX"
 export PATH="$PATH:$QT5BINDIR"
-export PKG_CONFIG_PATH="/usr/lib/pkgconfig:/opt/QT5/lib/pkgconfig"
+export PKG_CONFIG_PATH="/usr/lib/pkgconfig:/opt/qt5/lib/pkgconfig"
 CXXFLAGS=fPIC ./configure --prefix=/usr --with-qt5 --with-jack --with-pulseaudio &&
 make "-j`nproc`"
 sudo make install
