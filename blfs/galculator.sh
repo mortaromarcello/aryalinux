a8c73190721b4ad993d51c9d28359f48bb4a61a4
Chandrakant Singh <aryalinux11@gmail.com>
2016-10-25 14:43:03 +0530

    Fixed galculator

--- blfs/galculator.sh
+++ blfs/galculator.sh
@@ -6,8 +6,8 @@ set +h
 
 cd $SOURCE_DIR
 
-URL=http://sourceforge.net/projects/galculator/files/galculator/2.1.3/galculator-2.1.3.tar.bz2/download
-wget -c $URL -O galculator-2.1.3.tar.bz2
+URL=http://sourceforge.net/projects/galculator/files/galculator/2.1.3/galculator-2.1.3.tar.bz2
+wget -nc $URL
 TARBALL=galculator-2.1.3.tar.bz2
 DIRECTORY=`tar -tf $TARBALL | sed -e 's@/.*@@' | uniq `
 
