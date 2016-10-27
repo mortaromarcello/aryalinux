c1488577b7c77d9880523bdd42bf001d418ac8e8
Chandrakant Singh <aryalinux11@gmail.com>
2016-10-27 08:02:40 +0530

    Fixed net-tools

--- blfs/net-tools.sh
+++ blfs/net-tools.sh
@@ -26,7 +26,7 @@ then
 fi
 
 wget -nc http://ftp.lfs-matrix.net/pub/blfs/conglomeration/net-tools/net-tools-CVS_20101030.tar.gz || wget -nc http://ftp.osuosl.org/pub/blfs/conglomeration/net-tools/net-tools-CVS_20101030.tar.gz || wget -nc http://mirrors-usa.go-parts.com/blfs/conglomeration/net-tools/net-tools-CVS_20101030.tar.gz || wget -nc ftp://anduin.linuxfromscratch.org/BLFS/net-tools/net-tools-CVS_20101030.tar.gz || wget -nc ftp://ftp.lfs-matrix.net/pub/blfs/conglomeration/net-tools/net-tools-CVS_20101030.tar.gz || wget -nc ftp://ftp.osuosl.org/pub/blfs/conglomeration/net-tools/net-tools-CVS_20101030.tar.gz || wget -nc http://anduin.linuxfromscratch.org/BLFS/net-tools/net-tools-CVS_20101030.tar.gz
-wget -nc http://www.linuxfromscratch.org/patches/downloads/net-tools/net-tools-CVS_20101030-remove_dups-1.patch || wget -nc http://www.linuxfromscratch.org/patches/blfs/svn/net-tools-CVS_20101030-remove_dups-1.patch
+wget -nc https://raw.githubusercontent.com/FluidIdeas/patches/2016.11/net-tools-CVS_20101030-remove_dups-1.patch
 # wget -nc https://sourceforge.net/projects/net-tools/files/net-tools-1.60.tar.bz2
 wget -nc https://raw.githubusercontent.com/FluidIdeas/patches/2016.11/net-tools-1.60-kernel_headers-3.patch
 
@@ -39,7 +39,7 @@ cd $DIRECTORY
 
 whoami > /tmp/currentuser
 
-# patch -Np1 -i ../net-tools-CVS_20101030-remove_dups-1.patch &&
+patch -Np1 -i ../net-tools-CVS_20101030-remove_dups-1.patch &&
 patch -Np1 -i ../net-tools-1.60-kernel_headers-3.patch &&
 yes "" | make config &&
 make "-j`nproc`" || make
