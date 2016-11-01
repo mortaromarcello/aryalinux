4b1653813093ab689a3a6ffc6155c5d71c5c7b4b
Chandrakant Singh <aryalinux11@gmail.com>
2016-11-01 06:50:47 +0530

    Fixed backup.sh to include date properly

--- lfs/backup.sh
+++ lfs/backup.sh
@@ -19,7 +19,7 @@ fi
 
 pushd $LFS
 
-XZ_OPT=-9 tar --exclude=sources* --exclude=tools* --exclude=root/.ccache* --exclude=home/aryalinux/.ccache* --exclude=var/cache/alps/sources/* -cJvf ~/aryalinux-xfce-2016.11-`uname -m`.tar.xz * && XZ_OPT=-9 tar -cJvf ~/toolchain-2016.11-`uname -m`-`date -Idate`.tar.xz tools
+XZ_OPT=-9 tar --exclude=sources* --exclude=tools* --exclude=root/.ccache* --exclude=home/aryalinux/.ccache* --exclude=var/cache/alps/sources/* -cJvf ~/aryalinux-$OS_VERSION-`uname -m`-`date -I`.tar.xz * && XZ_OPT=-9 tar -cJvf ~/toolchain-$OS_VERSION-`uname -m`-`date -I`.tar.xz tools
 
 popd
 
