9420d7a7f028f1e608ab5811481d9f18b66bfb09
Chandrakant Singh <aryalinux11@gmail.com>
2016-11-02 20:57:27 +0530

    Fixed caja-extensions version

--- blfs/caja-extensions.sh
+++ blfs/caja-extensions.sh
@@ -13,7 +13,7 @@ VERSION="1.16.1"
 
 cd $SOURCE_DIR
 
-URL="http://pub.mate-desktop.org/releases/1.16/caja-extensions-1.16.1.tar.xz"
+URL="http://pub.mate-desktop.org/releases/1.16/caja-extensions-1.16.0.tar.xz"
 wget -nc $URL
 TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
 DIRECTORY=`tar -tf $TARBALL | cut -d/ -f1 | uniq`
--- blfs/x7driver.sh
+++ blfs/x7driver.sh
@@ -659,41 +659,6 @@ cd $SOURCE_DIR
 URL=https://github.com/i-rinat/libvdpau-va-gl/archive/v0.4.0.tar.gz
 
 wget -nc https://github.com/i-rinat/libvdpau-va-gl/archive/v0.4.0.tar.gz
-wget -nc ftp://ftp.lfs-matrix.net/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc http://ftp.lfs-matrix.net/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc ftp://ftp.osuosl.org/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc http://ftp.osuosl.org/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc http://mirrors-usa.go-parts.com/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc ftp://ftp.lfs-matrix.net/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc ftp://ftp.lfs-matrix.net/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc http://ftp.lfs-matrix.net/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc ftp://ftp.osuosl.org/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc http://ftp.osuosl.org/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc http://mirrors-usa.go-parts.com/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc http://ftp.lfs-matrix.net/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc ftp://ftp.lfs-matrix.net/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc http://ftp.lfs-matrix.net/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc ftp://ftp.osuosl.org/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc http://ftp.osuosl.org/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc http://mirrors-usa.go-parts.com/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc ftp://ftp.osuosl.org/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc ftp://ftp.lfs-matrix.net/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc http://ftp.lfs-matrix.net/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc ftp://ftp.osuosl.org/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc http://ftp.osuosl.org/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc http://mirrors-usa.go-parts.com/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc http://ftp.osuosl.org/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc ftp://ftp.lfs-matrix.net/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc http://ftp.lfs-matrix.net/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc ftp://ftp.osuosl.org/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc http://ftp.osuosl.org/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc http://mirrors-usa.go-parts.com/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc http://mirrors-usa.go-parts.com/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc ftp://ftp.lfs-matrix.net/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc http://ftp.lfs-matrix.net/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc ftp://ftp.osuosl.org/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc http://ftp.osuosl.org/pub/blfs/conglomeration/v0/v0.4.0.tar.gz
-wget -nc http://mirrors-usa.go-parts.com/blfs/conglomeration/v0/v0.4.0.tar.gz
 
 TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
 DIRECTORY=`tar tf $TARBALL | cut -d/ -f1 | uniq`
