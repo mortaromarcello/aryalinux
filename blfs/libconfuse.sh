a1aa4a1e46af3dee9cc9837b6053fd72e73eb281
Chandrakant Singh <aryalinux11@gmail.com>
2016-11-19 10:04:40 +0530

    Fixed libconfuse

--- blfs/libconfuse.sh
+++ blfs/libconfuse.sh
@@ -22,7 +22,6 @@ DIRECTORY=$(tar tf $TARBALL | cut -d/ -f1 | uniq)
 tar xf $TARBALL
 cd $DIRECTORY
 
-./autogen.sh --prefix=/usr &&
 ./configure --prefix=/usr &&
 make "-j$(nproc)"
 sudo make install
