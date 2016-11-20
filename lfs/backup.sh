0a54f47179e818790f6f21aa6344fa751407f55c
Chandrakant Singh <aryalinux11@gmail.com>
2016-11-20 19:18:51 +0530

    Fixed backup

--- lfs/backup.sh
+++ lfs/backup.sh
@@ -24,7 +24,7 @@ fi
 
 pushd /
 
-if [ -z "`ls $LFS/sources/ | grep -E 'aryalinux-$OS_VERSION-$LABEL'`" ]
+if [ -z $(ls $LFS/sources/aryalinux-$OS_VERSION-$LABEL*.tar.gz) ]
 then
 	GZIP=-9 tar --exclude="/mnt/lfs/sources" --exclude="/mnt/lfs/tools" --exclude="/mnt/lfs/root/.ccache" --exclude="/mnt/lfs/home/aryalinux/.ccache" --exclude="/mnt/lfs/var/cache/alps/binaries" --exclude="/mnt/lfs/var/cache/alps/sources" -czvf $LFS/sources/aryalinux-$OS_VERSION-$LABEL-`uname -m`-`date -I`.tar.gz $LFS
 	if [ "$TOOLCHAIN_BACKUP" == "y" ] || [ "$TOOLCHAIN_BACKUP" == "Y" ] && [ -z $(ls $LFS/sources/toolchain*.tar.xz) ] ; then
