6e24d0ae0d57e4bd3b3d59ae4b39cf67ec0f6226
Chandrakant Singh <aryalinux11@gmail.com>
2016-11-21 00:44:53 +0530

    Fixed backup.sh

--- lfs/backup.sh
+++ lfs/backup.sh
@@ -23,17 +23,12 @@ fi
 
 pushd /
 
-if [ -z $(ls $LFS/sources/aryalinux-$OS_VERSION-$LABEL*.tar.gz) ]
+if [ ! -f $LFS/sources/aryalinux-$OS_VERSION-$LABEL-$(uname -m).tar.gz) ]
 then
-	GZIP=-9 tar --exclude="/mnt/lfs/sources" --exclude="/mnt/lfs/tools" --exclude="/mnt/lfs/root/.ccache" --exclude="/mnt/lfs/home/aryalinux/.ccache" --exclude="/mnt/lfs/var/cache/alps/binaries" --exclude="/mnt/lfs/var/cache/alps/sources" -czvf $LFS/sources/aryalinux-$OS_VERSION-$LABEL-`uname -m`-`date -I`.tar.gz $LFS
-	if [ "$TOOLCHAIN_BACKUP" == "y" ] || [ "$TOOLCHAIN_BACKUP" == "Y" ] && [ -z $(ls $LFS/sources/toolchain*.tar.xz) ] ; then
-		XZ_OPT=-9 tar -cJvf $LFS/sources/toolchain-$OS_VERSION-`uname -m`-`date -I`.tar.xz $LFS/tools
+	GZIP=-9 tar --exclude="/mnt/lfs/sources" --exclude="/mnt/lfs/tools" --exclude="/mnt/lfs/root/.ccache" --exclude="/mnt/lfs/home/aryalinux/.ccache" --exclude="/mnt/lfs/var/cache/alps/binaries" --exclude="/mnt/lfs/var/cache/alps/sources" -czvf $LFS/sources/aryalinux-$OS_VERSION-$LABEL-$(uname -m).tar.gz $LFS
+	if [ "$TOOLCHAIN_BACKUP" == "y" ] || [ "$TOOLCHAIN_BACKUP" == "Y" ] && [ ! -f $LFS/sources/toolchain-$OS_VERSION-$(uname -m).tar.xz) ] ; then
+		XZ_OPT=-9 tar -cJvf $LFS/sources/toolchain-$OS_VERSION-$(uname -m).tar.xz $LFS/tools
 	fi
 fi
-
-#mkdir -pv ~/packages
-#if [ -d /mnt/lfs/var/cache/alps/binaries/ ] && [ $(ls -A /mnt/lfs/var/cache/alps/binaries/) ] ; then
-#	cp -v /mnt/lfs/var/cache/alps/binaries/* ~/packages
-#fi
 
 popd
