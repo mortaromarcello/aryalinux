e981518040c219c04417bcb777509ba5fae0f94d
Chandrakant Singh <aryalinux11@gmail.com>
2016-11-21 08:09:47 +0530

    More fixes. Testing...

--- lfs/final-stage.sh
+++ lfs/final-stage.sh
@@ -4,19 +4,16 @@ set -e
 set +h
 
 cd /sources/
+. ./build-properties
 
 if ! grep "root-and-admin-passwords" /sources/build-log &> /dev/null
 then
 
-echo "Root password : $1"
-echo "User password : $2"
-
 echo "Setting the password for root :"
-yes "$1" | passwd root
+yes "$1" | passwd
 
-clear
 echo "Setting the password for $USERNAME :"
-yes "$2" | passwd $USERNAME
+yes "$2" | passwd "$USERNAME"
 
 echo "Done with the build process. You may now exit by entering the following :"
 echo ""
