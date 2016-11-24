bba4a8edb2f6b995bf28803eea2c7b55976c0eaf
Marcello Mortaro <marcellomortaro@localhost.localdomain>
2016-11-24 10:56:55 +0100

    new file:   lfs/final-system/083-systemd.sh.orig 	new file:   lfs/final-system/099-dbus.sh.orig 	new file:   lfs/final-system/099-sysklogd.sh 	new file:   lfs/final-system/100-sysvinit.sh 	new file:   lfs/final-system/102-eudev.sh 	new file:   lfs/final-system/103-util-linux.sh 	new file:   lfs/final-system/104-man-db.sh 	new file:   lfs/final-system/105-tar.sh 	new file:   lfs/final-system/106-texinfo.sh 	new file:   lfs/final-system/107-vim.sh 	new file:   lfs/template

---
+++ lfs/final-system/102-eudev.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+set -e
+set +h
+
+. /sources/build-properties
+
+if [ "x$MULTICORE" == "xy" ] || [ "x$MULTICORE" == "xY" ]
+then
+	export MAKEFLAGS="-j `nproc`"
+fi
+
+SOURCE_DIR="/sources"
+LOGFILE="/sources/build-log"
+STEPNAME="101-eudev.sh"
+TARBALL="eudev-3.2.tar.gz"
+
+if ! grep "$STEPNAME" $LOGFILE &> /dev/null
+then
+
+cd $SOURCE_DIR
+
+if [ "$TARBALL" != "" ]
+	DIRECTORY=`tar -tf $TARBALL | cut -d/ -f1 | uniq`
+	tar xf $TARBALL
+	cd $DIRECTORY
+fi
+
+sed -r -i 's|/usr(/bin/test)|\1|' test/udev-test.pl
+cat > config.cache << "EOF"
+HAVE_BLKID=1
+BLKID_LIBS="-lblkid"
+BLKID_CFLAGS="-I/tools/include"
+EOF
+
+./configure --prefix=/usr           \
+            --bindir=/sbin          \
+            --sbindir=/sbin         \
+            --libdir=/usr/lib       \
+            --sysconfdir=/etc       \
+            --libexecdir=/lib       \
+            --with-rootprefix=      \
+            --with-rootlibdir=/lib  \
+            --enable-manpages       \
+            --disable-static        \
+            --config-cache
+
+LIBRARY_PATH=/tools/lib make
+
+mkdir -pv /lib/udev/rules.d
+mkdir -pv /etc/udev/rules.d
+make LD_LIBRARY_PATH=/tools/lib install
+tar -xvf ../udev-lfs-20140408.tar.bz2
+make -f udev-lfs-20140408/Makefile.lfs install
+
+LD_LIBRARY_PATH=/tools/lib udevadm hwdb --update
+
+
+cd $SOURCE_DIR
+if [ "$TARBALL" != "" ]
+then
+	rm -rf $DIRECTORY
+	rm -rf {gcc,glibc,binutils}-build
+fi
+
+echo "$STEPNAME" | tee -a $LOGFILE
+
+fi
