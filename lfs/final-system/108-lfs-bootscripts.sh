7e05821d68be2dfaabf7127fc6fec980ef62b6ff
Marcello Mortaro <marcellomortaro@localhost.localdomain>
2016-11-25 07:20:16 +0100

    new file:   lfs/final-system/108-lfs-bootscripts.sh

---
+++ lfs/final-system/108-lfs-bootscripts.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+set +h
+
+. /sources/build-properties
+
+if [ "x$MULTICORE" == "xy" ] || [ "x$MULTICORE" == "xY" ]
+then
+	export MAKEFLAGS="-j `nproc`"
+fi
+
+SOURCE_DIR="/sources"
+LOGFILE="/sources/build-log"
+STEPNAME="108-lfs-bootscripts.sh"
+TARBALL="lfs-bootscripts-20150222"
+
+if ! grep "$STEPNAME" $LOGFILE &> /dev/null
+then
+
+cd $SOURCE_DIR
+
+if [ "$TARBALL" != "" ]
+then
+	DIRECTORY=`tar -tf $TARBALL | cut -d/ -f1 | uniq`
+	tar xf $TARBALL
+	cd $DIRECTORY
+fi
+
+make install
+
+cd $SOURCE_DIR
+if [ "$TARBALL" != "" ]
+then
+	rm -rf $DIRECTORY
+	rm -rf {gcc,glibc,binutils}-build
+fi
+
+echo "$STEPNAME" | tee -a $LOGFILE
+
+fi
