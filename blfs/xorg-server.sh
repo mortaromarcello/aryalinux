2f707c4e609aff54da9085d86e1739de23d48be1
Marcello Mortaro <marcellomortaro@localhost.localdomain>
2016-12-03 13:13:28 +0100

    modified:   blfs/xorg-server.sh

--- blfs/xorg-server.sh
+++ blfs/xorg-server.sh
@@ -9,7 +9,7 @@ set +h
 SOURCE_ONLY=n
 DESCRIPTION="br3ak The Xorg Server is the core of thebr3ak X Window system.br3ak"
 SECTION="x"
-VERSION=1.19.0
+VERSION=1.18.4
 NAME="xorg-server"
 
 #REQ:openssl
@@ -32,12 +32,12 @@ NAME="xorg-server"
 
 cd $SOURCE_DIR
 
-URL=http://ftp.x.org/pub/individual/xserver/xorg-server-1.19.0.tar.bz2
+URL=http://ftp.x.org/pub/individual/xserver/xorg-server-1.18.4.tar.bz2
 
 if [ ! -z $URL ]
 then
-wget -nc http://ftp.x.org/pub/individual/xserver/xorg-server-1.19.0.tar.bz2 || wget -nc ftp://ftp.lfs-matrix.net/pub/blfs/conglomeration/Xorg/xorg-server-1.19.0.tar.bz2 || wget -nc http://ftp.lfs-matrix.net/pub/blfs/conglomeration/Xorg/xorg-server-1.19.0.tar.bz2 || wget -nc ftp://ftp.x.org/pub/individual/xserver/xorg-server-1.19.0.tar.bz2 || wget -nc http://ftp.osuosl.org/pub/blfs/conglomeration/Xorg/xorg-server-1.19.0.tar.bz2 || wget -nc http://mirrors-usa.go-parts.com/blfs/conglomeration/Xorg/xorg-server-1.19.0.tar.bz2 || wget -nc ftp://ftp.osuosl.org/pub/blfs/conglomeration/Xorg/xorg-server-1.19.0.tar.bz2
-wget -nc http://www.linuxfromscratch.org/patches/downloads/xorg-server/xorg-server-1.19.0-add_prime_support-1.patch || wget -nc http://www.linuxfromscratch.org/patches/blfs/svn/xorg-server-1.19.0-add_prime_support-1.patch
+wget -nc http://ftp.x.org/pub/individual/xserver/xorg-server-1.18.4.tar.bz2 || wget -nc ftp://ftp.lfs-matrix.net/pub/blfs/conglomeration/Xorg/xorg-server-1.18.4.tar.bz2 || wget -nc http://ftp.lfs-matrix.net/pub/blfs/conglomeration/Xorg/xorg-server-1.18.4.tar.bz2 || wget -nc ftp://ftp.x.org/pub/individual/xserver/xorg-server-1.18.4.tar.bz2 || wget -nc http://ftp.osuosl.org/pub/blfs/conglomeration/Xorg/xorg-server-1.18.4.tar.bz2 || wget -nc http://mirrors-usa.go-parts.com/blfs/conglomeration/Xorg/xorg-server-1.18.4.tar.bz2 || wget -nc ftp://ftp.osuosl.org/pub/blfs/conglomeration/Xorg/xorg-server-1.18.4.tar.bz2
+wget -nc http://www.linuxfromscratch.org/patches/downloads/xorg-server/xorg-server-1.18.4-add_prime_support-1.patch || wget -nc http://www.linuxfromscratch.org/patches/blfs/svn/xorg-server-1.18.4-add_prime_support-1.patch
 
 TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
 if [ -z $(echo $TARBALL | grep ".zip$") ]; then
@@ -55,7 +55,7 @@ whoami > /tmp/currentuser
 export XORG_PREFIX=/usr
 export XORG_CONFIG="--prefix=$XORG_PREFIX --sysconfdir=/etc --localstatedir=/var --disable-static"
 
-patch -Np1 -i ../xorg-server-1.19.0-add_prime_support-1.patch
+patch -Np1 -i ../xorg-server-1.18.4-add_prime_support-1.patch
 
 
 ./configure $XORG_CONFIG             \
