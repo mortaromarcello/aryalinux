1c7f378509ec9dc83b4594eab993598d4b6d04e7
Marcello Mortaro <marcellomortaro@localhost.localdomain>
2016-12-05 06:10:15 +0100

    modified:   lfs/stage7.sh

--- lfs/stage7.sh
+++ lfs/stage7.sh
@@ -83,6 +83,15 @@ KERNEL=="video*", ATTRS{device}=="0x036f", ATTRS{vendor}=="0x109e", \
 
 EOF
 
+cat > /etc/sysconfig/clock << "EOF"
+# Begin /etc/sysconfig/clock
+UTC=1
+# Set this to any options you might need to give to hwclock,
+# such as machine hardware clock type for Alphas.
+CLOCKPARAMS=
+# End /etc/sysconfig/clock
+EOF
+
 cat > /etc/adjtime << "EOF"
 0.0 0 0.0
 0
