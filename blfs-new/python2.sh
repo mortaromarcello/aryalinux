2b15c8a2b6135f1b8be775838be18cfb1ca0cedc
Marcello Mortaro <mortaromarcello@gmail.com>
2016-12-22 23:19:02 +0100

    modified:   ../blfs-new/python2.sh 	modified:   alps

--- blfs-new/python2.sh
+++ blfs-new/python2.sh
@@ -66,6 +66,33 @@ function build() {
         -xvf ../python-2.7.12-docs-html.tar.bz2 &&
     find $PKG/usr/share/doc/python-2.7.12 -type d -exec chmod 0755 {} \; &&
     find $PKG/usr/share/doc/python-2.7.12 -type f -exec chmod 0644 {} \;
+    cat > $PKG/etc/pythonrc.py << "EOF"
+# Add auto-completion and a stored history file of commands to your Python
+# interactive interpreter. Requires Python 2.0+, readline. Autocomplete is
+# bound to the Esc key by default (you can change it - see readline docs).
+#
+# Store the file in ~/.pystartup, and set an environment variable to point
+# to it:  "export PYTHONSTARTUP=~/.pystartup" in bash.
+
+import atexit
+import os
+import readline
+
+historyPath = os.path.expanduser("~/.pyhistory")
+
+
+def save_history(historyPath=historyPath):
+    import readline
+    readline.write_history_file(historyPath)
+
+if os.path.exists(historyPath):
+    readline.read_history_file(historyPath)
+
+atexit.register(save_history)
+readline.parse_and_bind("meta: complete")
+del os, atexit, readline, save_history, historyPath
+
+EOF
 }
 
 function package() {
