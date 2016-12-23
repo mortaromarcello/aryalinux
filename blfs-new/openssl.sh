9d46468866562b338c464ee2d7a64dddc11a7aa7
Marcello Mortaro <mortaromarcello@gmail.com>
2016-12-23 08:07:56 +0100

    modified:   blfs-new/openssl.sh

--- blfs-new/openssl.sh
+++ blfs-new/openssl.sh
@@ -9,15 +9,13 @@
 set -e
 set +h
 
-#. /etc/alps/alps.conf
-#. /var/lib/alps/functions
-
 SOURCE_ONLY=n
 DESCRIPTION="\n The OpenSSL package contains\n management tools and libraries relating to cryptography. These are\n useful for providing cryptography functions to other packages, such\n as OpenSSH, email applications and\n web browsers (for accessing HTTPS sites).\n"
 SECTION="postlfs"
 VERSION=1.0.2j
 NAME="openssl"
 PKGNAME=$NAME
+REVISION=1
 
 #OPT:mitkrb
 
@@ -62,15 +60,17 @@ function package() {
     #chown -R root:root usr/bin
     #gzip -9 $PKG/usr/share/man/man?/*.?
     cd $PKG
-    find . -type f -name "*"|sed 's/^.//' > $START/$PKGNAME-$VERSION-$ARCH-1.files
-    find . -type d -name "*"|sed 's/^.//' >> $START/$PKGNAME-$VERSION-$ARCH-1.files
+    find . -type f -name "*"|sed 's/^.//' > $START/$PKGNAME-$VERSION-$ARCH$REVISION.files
+    find . -type d -name "*"|sed 's/^.//' >> $START/$PKGNAME-$VERSION-$ARCH$REVISION.files
+    gzip -f $START/$PKGNAME-$VERSION-$ARCH-$REVISION.files > $START/$PKGNAME-$VERSION-$ARCH-$REVISION.files.gz
     mkdir $PKG/install
     echo -e $DESCRIPTION > $PKG/install/blfs-desc
+    mv -v $START/$PKGNAME-$VERSION-$ARCH-$REVISION.files.gz $PKG/install/
     cat > $PKG/install/doinst.sh << "EOF"
 echo -e "Non ho niente da fare!"
 EOF
-    tar cvvf - . --format gnu --xform 'sx^\./\(.\)x\1x' --show-stored-names --group 0 --owner 0 | gzip > $START/$PKGNAME-$VERSION-$ARCH-1.tgz
-    echo "blfs package \"$PKGNAME-$VERSION-$ARCH-1.tgz\" created."
+    tar cvvf - . --format gnu --xform 'sx^\./\(.\)x\1x' --show-stored-names --group 0 --owner 0 | gzip > $START/$PKGNAME-$VERSION-$ARCH$REVISION.tgz
+    echo "blfs package \"$PKGNAME-$VERSION-$ARCH$REVISION.tgz\" created."
 }
 build
 package
