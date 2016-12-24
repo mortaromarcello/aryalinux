e3b2c445a36e6d409036d011f647e9960de34444
Marcello Mortaro <mortaromarcello@gmail.com>
2016-12-24 10:26:25 +0100

    new file:   blfs-new/libICE.sh 	new file:   blfs-new/libXcomposite.sh 	new file:   blfs-new/libXcursor.sh 	new file:   blfs-new/libXdamage.sh 	new file:   blfs-new/libXfixes.sh 	new file:   blfs-new/libXft.sh 	new file:   blfs-new/libXi.sh 	new file:   blfs-new/libXinerama.sh 	new file:   blfs-new/libXrandr.sh 	new file:   blfs-new/libXrender.sh 	new file:   blfs-new/libXres.sh 	new file:   blfs-new/libXtst.sh 	new file:   blfs-new/libXv.sh 	new file:   blfs-new/libXvMC.sh 	new file:   blfs-new/libXxf86dga.sh 	new file:   blfs-new/libXxf86vm.sh

---
+++ blfs-new/libXres.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+#############################################################################
+## Name:                                                                   ##
+## Version:                                                                ##
+## Packager: Mortaro Marcello (mortaromarcello@gmail.com)                  ##
+## Homepage:                                                               ##
+#############################################################################
+set -e
+set +h
+
+SOURCE_ONLY=n
+DESCRIPTION="\n The Xorg libXres library provide library\n routines that are used within all X Window applications.\n"
+SECTION="x"
+VERSION=1.0.7
+NAME="libXres"
+PKGNAME=$NAME
+REVISION=1
+
+ARCH=`uname -m`
+
+START=`pwd`
+PKG=$START/pkg
+SRC=$START/work
+
+function unzip_file()
+{
+	dir_name=$(unzip_dirname $1 $2)
+	echo $dir_name
+	if [ `echo $dir_name | grep "extracted$"` ]
+	then
+		echo "Create and extract..."
+		mkdir $dir_name
+		cp $1 $dir_name
+		cd $dir_name
+		unzip $1
+		cd ..
+	else
+		echo "Just Extract..."
+		unzip $1
+	fi
+}
+function build() {
+    mkdir -vp $PKG $SRC
+    cd $PKG
+    case $(uname -m) in
+        x86_64)
+            mkdir -vp lib
+            ln -sv lib lib64
+            mkdir -vp usr/lib
+            ln -sv lib usr/lib64
+            mkdir -vp usr/local/lib
+            ln -sv lib usr/local/lib64 ;;
+    esac
+    cd $SRC
+    URL=http://ftp.x.org/pub/individual/lib/$NAME-$VERSION.tar.bz2
+    if [ ! -z $URL ]; then
+        wget -nc http://ftp.x.org/pub/individual/lib/$NAME-$VERSION.tar.bz2
+        TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
+        if [ -z $(echo $TARBALL | grep ".zip$") ]; then
+            DIRECTORY=`tar tf $TARBALL | cut -d/ -f1 | uniq | grep -v "^\.$"`
+            tar --no-overwrite-dir -xvf $TARBALL
+        else
+            DIRECTORY=$(unzip_dirname $TARBALL $NAME)
+            unzip_file $TARBALL $NAME
+        fi
+        cd $DIRECTORY
+    fi
+    export XORG_PREFIX=/usr
+    export XORG_CONFIG="--prefix=$XORG_PREFIX --sysconfdir=/etc --localstatedir=/var --disable-static"
+    ./configure $XORG_CONFIG
+    make DESTDIR=$PKG install
+}
+
+function package() {
+    gzip -9 $PKG/usr/share/man/man?/*.?
+    cd $PKG
+    find . -type f -name "*"|sed 's/^.//' > $START/$PKGNAME-$VERSION-$ARCH-$REVISION.files
+    find . -type d -name "*"|sed 's/^.//' >> $START/$PKGNAME-$VERSION-$ARCH-$REVISION.files
+    gzip -f $START/$PKGNAME-$VERSION-$ARCH-$REVISION.files > $START/$PKGNAME-$VERSION-$ARCH-$REVISION.files.gz
+    mkdir -vp $PKG/install
+    mv -v $START/$PKGNAME-$VERSION-$ARCH-$REVISION.files.gz $PKG/install/
+    echo -e $DESCRIPTION > $PKG/install/blfs-desc
+    cat > $PKG/install/doinst.sh << "EOF"
+#!/bin/sh
+echo -e "Non ho niente da fare!"
+EOF
+    tar cvvf - . --format gnu --xform 'sx^\./\(.\)x\1x' --show-stored-names --group 0 --owner 0 | gzip > $START/$PKGNAME-$VERSION-$ARCH-$REVISION.tgz
+    echo "blfs package \"$PKGNAME-$VERSION-$ARCH-$REVISION.tgz\" created."
+}
+build
+package
+
+if [ ! -z $URL ]; then
+    cd $START && rm -vrf $PKG && rm -vrf $SRC
+fi
