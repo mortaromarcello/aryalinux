b724d6bff4491313c47fc2e0fa27f8c330facb25
Marcello Mortaro <mortaromarcello@gmail.com>
2017-01-06 23:24:45 +0100

    modified:   blfs-new/x264.sh 	modified:   blfs-new/x265.sh

--- blfs-new/x264.sh
+++ blfs-new/x264.sh
@@ -95,8 +95,6 @@ function build() {
 }
 
 function package() {
-    strip -s $PKG/usr/bin/*
-    gzip -9 $PKG/usr/share/man/man?/*.?
     cd $PKG
     find . -type f -name "*"|sed 's/^.//' > $START/$PKGNAME-$VERSION-$ARCH-$REVISION.files
     find . -type d -name "*"|sed 's/^.//' >> $START/$PKGNAME-$VERSION-$ARCH-$REVISION.files
