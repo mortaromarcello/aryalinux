f0110a15025558a19cd7cbc04219a1b656a01934
Marcello Mortaro <mortaromarcello@gmail.com>
2017-01-06 22:57:09 +0100

    modified:   blfs-new/libvorbis.sh

--- blfs-new/libvorbis.sh
+++ blfs-new/libvorbis.sh
@@ -77,9 +77,9 @@ function build() {
             ln -sv lib usr/local/lib64 ;;
     esac
     cd $SRC
-    URL=
+    URL=http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.5.tar.xz
     if [ ! -z $URL ]; then
-        wget 
+        wget -nc http://ftp.osuosl.org/pub/blfs/conglomeration/libvorbis/libvorbis-1.3.5.tar.xz || wget -nc http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.5.tar.xz || wget -nc http://ftp.lfs-matrix.net/pub/blfs/conglomeration/libvorbis/libvorbis-1.3.5.tar.xz || wget -nc ftp://ftp.lfs-matrix.net/pub/blfs/conglomeration/libvorbis/libvorbis-1.3.5.tar.xz || wget -nc ftp://ftp.osuosl.org/pub/blfs/conglomeration/libvorbis/libvorbis-1.3.5.tar.xz || wget -nc http://mirrors-usa.go-parts.com/blfs/conglomeration/libvorbis/libvorbis-1.3.5.tar.xz
         TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
         if [ -z $(echo $TARBALL | grep ".zip$") ]; then
             DIRECTORY=`tar tf $TARBALL | cut -d/ -f1 | uniq | grep -v "^\.$"`
