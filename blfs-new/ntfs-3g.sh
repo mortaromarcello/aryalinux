2dcc97e8c086348d6759ecc7e5aecf2158367d5b
Marcello Mortaro <mortaromarcello@gmail.com>
2017-01-23 10:24:15 +0100

    new file:   blfs-new/ntfs-3g.sh

---
+++ blfs-new/ntfs-3g.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+#############################################################################
+## Name:                                                                   ##
+## Version:                                                                ##
+## Packager: Mortaro Marcello (mortaromarcello@gmail.com)                  ##
+## Homepage:                                                               ##
+#############################################################################
+set -e
+set +h
+
+SOURCE_ONLY=n
+DESCRIPTION="The Ntfs-3g package contains a stable, read-write open source driver for NTFS partitions. NTFS partitions are used by newer Microsoft operating systems. Ntfs-3g enables you to mount NTFS partitions in read-write mode from your Linux system. It uses the FUSE kernel module to be able to implement NTFS support in user space."
+SECTION="postlfs"
+VERSION=2014.2.15
+NAME="ntfs-3g"
+PKGNAME=$NAME
+REVISION=1
+URL=http://tuxera.com/opensource/ntfs-3g_ntfsprogs-2014.2.15.tgz
+
+#OPT:fuse
+
+ARCH=`uname -m`
+
+START=`pwd`
+PKG=$START/pkg
+SRC=$START/work
+
+function unzip_dirname()
+{
+    dirname="$2-extracted"
+    unzip -o -q $1 -d $dirname
+    if [ "$(ls $dirname | wc -w)" == "1" ]; then
+        echo "$(ls $dirname)"
+    else
+        echo "$dirname"
+    fi
+    rm -rf $dirname
+}
+
+function unzip_file()
+{
+    dir_name=$(unzip_dirname $1 $2)
+    echo $dir_name
+    if [ `echo $dir_name | grep "extracted$"` ]
+    then
+        echo "Create and extract..."
+        mkdir $dir_name
+        cp $1 $dir_name
+        cd $dir_name
+        unzip $1
+        cd ..
+    else
+        echo "Just Extract..."
+        unzip $1
+    fi
+}
+
+function build() {
+    if [ -d $PKG ]; then
+        rm -rvf $PKG
+    fi
+    if [ -d $SRC ]; then
+        rm -rvf $SRC
+    fi
+    mkdir -vp $PKG $SRC
+    cd $PKG
+    case $(uname -m) in
+        x86_64)
+            mkdir -vp lib
+            ln -sv lib lib64
+            mkdir -vp usr/lib
+            ln -sv lib usr/lib64
+            mkdir -vp usr/local/lib
+            ln -sv lib usr/local/lib64 ;;
+    esac
+    cd $SRC
+    if [ ! -z $URL ]; then
+        wget 
+        TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
+        if [ -z $(echo $TARBALL | grep ".zip$") ]; then
+            DIRECTORY=`tar tf $TARBALL | cut -d/ -f1 | uniq | grep -v "^\.$"`
+            tar --no-overwrite-dir -xvf $TARBALL
+        else
+            DIRECTORY=$(unzip_dirname $TARBALL $NAME)
+            unzip_file $TARBALL $NAME
+        fi
+        cd $DIRECTORY
+    fi
+    ./configure --prefix=/usr --disable-static &&
+    make
+    make DESTDIR=$PKG install &&
+    ln -sv ../bin/ntfs-3g $PKG/sbin/mount.ntfs &&
+    ln -sv $PKG/usr/share/man/man8/{ntfs-3g,mount.ntfs}.8
+    chmod -v 4755 $PKG/sbin/mount.ntfs
+}
+
+function package() {
+    strip -s $PKG/sbin/*
+    cd $PKG
+    find . -type f -name "*"|sed 's/^.//' > $START/$PKGNAME-$VERSION-$ARCH-$REVISION.files
+    find . -type d -name "*"|sed 's/^.//' >> $START/$PKGNAME-$VERSION-$ARCH-$REVISION.files
+    gzip -f $START/$PKGNAME-$VERSION-$ARCH-$REVISION.files > $START/$PKGNAME-$VERSION-$ARCH-$REVISION.files.gz
+    mkdir -vp $PKG/install
+    mv -v $START/$PKGNAME-$VERSION-$ARCH-$REVISION.files.gz $PKG/install/
+    echo -e $DESCRIPTION > $PKG/install/blfs-desc
+    cat > $PKG/install/doinst.sh << "EOF"
+#!/bin/sh
+echo -e "Non ho niente da fare!"
+EOF
+    cat > $PKG/install/$PKGNAME-$VERSION-$ARCH-$REVISION.preremove << "EOF"
+#!/bin/sh
+echo -e "Non ho niente da fare!"
+EOF
+    cat > $PKG/install/$PKGNAME-$VERSION-$ARCH-$REVISION.postremove << "EOF"
+#!/bin/sh
+echo -e "Non ho niente da fare!"
+EOF
+    tar cvvf - . --format gnu --xform 'sx^\./\(.\)x\1x' --show-stored-names --group 0 --owner 0 | gzip > $START/$PKGNAME-$VERSION-$ARCH-$REVISION.tgz
+    echo "blfs package \"$PKGNAME-$VERSION-$ARCH-$REVISION.tgz\" created."
+}
+
+build
+package
+
+if [ ! -z $URL ]; then
+    cd $START && rm -vrf $PKG && rm -vrf $SRC
+fi
