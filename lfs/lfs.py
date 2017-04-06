07429dbfaffaadd1cb81eecdf97a6d24a5c44dbf
Marcello Mortaro <mortaromarcello@gmail.com>
2017-04-06 06:44:54 +0200

    modified:   lfs/lfs.py

--- lfs/lfs.py
+++ lfs/lfs.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
-import sys, os, time, parted, libmount, shutil, stat
+import sys, os, parted, shutil, libmount, stat, glob, pwd
 import subprocess as sproc
+from pychroot import Chroot
+
 try:
     import wget
 except ImportError:
     print ("not import wget. Exit")
-    exit
+    exit(-1)
 try:
     import pytz
 except ImportError:
     print ("not import pytz. Exit")
-    exit
+    exit(-1)
 
 #-----------------------data--------------------------------------------
 WGET_LIST=["http://download.savannah.gnu.org/releases/acl/acl-2.2.52.src.tar.gz",
@@ -91,21 +93,61 @@ WGET_LIST=["http://download.savannah.gnu.org/releases/acl/acl-2.2.52.src.tar.gz"
 "http://www.linuxfromscratch.org/patches/lfs/7.10/glibc-2.24-fhs-1.patch",
 "http://www.linuxfromscratch.org/patches/lfs/7.10/kbd-2.0.3-backspace-1.patch",
 "http://www.linuxfromscratch.org/patches/lfs/7.10/readline-6.3-upstream_fixes-3.patch",
-"http://www.linuxfromscratch.org/patches/lfs/7.10/sysvinit-2.88dsf-consolidated-1.patch"
+"http://www.linuxfromscratch.org/patches/lfs/7.10/sysvinit-2.88dsf-consolidated-1.patch",
+############# Additional downloads###########
+"http://pkgs.fedoraproject.org/lookaside/pkgs/dosfstools/dosfstools-3.0.26.tar.xz/45012f5f56f2aae3afcd62120b9e5a08/dosfstools-3.0.26.tar.xz",
+"http://ftp.gnu.org/gnu/which/which-2.21.tar.gz",
+"http://ftp.de.debian.org/debian/pool/main/o/os-prober/os-prober_1.71.tar.xz",
+"http://pkgs.fedoraproject.org/repo/pkgs/efivar/efivar-0.23.tar.bz2/bff7aa95fdb2f5d79f4aa9721dca2bbd/efivar-0.23.tar.bz2",
+"http://pkgs.fedoraproject.org/repo/pkgs/efibootmgr/efibootmgr-0.12.tar.bz2/6647f5cd807bc8484135ba74fcbcc39a/efibootmgr-0.12.tar.bz2",
+"http://downloads.sourceforge.net/freetype/freetype-2.6.3.tar.bz2",
+"http://unifoundry.com/pub/unifont-7.0.05/font-builds/unifont-7.0.05.pcf.gz",
+"http://pkgs.fedoraproject.org/repo/pkgs/pciutils/pciutils-3.4.1.tar.gz/acc91d632dbc98f624a8e57b4e478160/pciutils-3.4.1.tar.gz",
+"http://pkgs.fedoraproject.org/repo/pkgs/popt/popt-1.16.tar.gz/3743beefa3dd6247a73f8f7a32c14c33/popt-1.16.tar.gz",
+"http://pkgs.fedoraproject.org/repo/pkgs/linux-firmware/linux-firmware-20160321.tar.gz/46a7ec26850851c9da93ba84dd14fc71/linux-firmware-20160321.tar.gz",
+"http://ftp.gnu.org/pub/gnu/cpio/cpio-2.12.tar.bz2",
+"http://pkgs.fedoraproject.org/repo/pkgs/redhat-lsb/lsb-release-1.4.tar.gz/30537ef5a01e0ca94b7b8eb6a36bb1e4/lsb-release-1.4.tar.gz",
+"https://busybox.net/downloads/fixes-1.20.2/busybox-1.20.2-sys-resource.patch",
+"https://busybox.net/downloads/busybox-1.20.2.tar.bz2",
+"https://ftp.gnu.org/gnu/nettle/nettle-3.1.1.tar.gz",
+"http://ftp.gnu.org/gnu/libtasn1/libtasn1-4.5.tar.gz",
+"http://p11-glue.freedesktop.org/releases/p11-kit-0.23.2.tar.gz",
+"ftp://ftp.gnutls.org/gcrypt/gnutls/v3.4/gnutls-3.4.3.tar.xz",
+"http://ftp.gnu.org/gnu/wget/wget-1.16.3.tar.xz",
+"http://www.sudo.ws/dist/sudo-1.8.16.tar.gz",
+"ftp://sourceware.org/pub/libffi/libffi-3.2.1.tar.gz",
+"http://sqlite.org/2016/sqlite-autoconf-3140100.tar.gz",
+"https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tar.xz",
+"https://ftp.dlitz.net/pub/dlitz/crypto/pycrypto/pycrypto-2.6.1.tar.gz",
+"http://anduin.linuxfromscratch.org/sources/other/certdata.txt",
+"http://www.openssl.org/source/openssl-1.0.2j.tar.gz",
+"http://www.kernel.org/pub/linux/utils/boot/syslinux/syslinux-4.06.tar.xz",
+#"http://gondor.apana.org.au/~herbert/dash/files/dash-0.5.8.tar.gz",
+"https://www.kernel.org/pub/linux/utils/boot/dracut/dracut-044.tar.xz",
+"ftp://sources.redhat.com/pub/lvm2/releases/LVM2.2.02.155.tgz"
 ]
 pushstack = list()
 LFS = "/mnt/lfs"
+LC_ALL='POSIX'
+LFS_TGT=sproc.check_output("echo $(uname -m)-lfs-linux-gnu", shell=True).strip('\n')
 TIMEZONE = "Europe/Rome"
 FS = "ext4"
 ROOT_PART = "" #"/dev/sdc5"
 SWAP_PART = "" #"/dev/sdxx"
 HOME_PART = "" #
+FORMAT_HOME = False
 
 #---------------------------functions----------------------------------#
+def demote(user_uid, user_gid):
+    def result():
+        os.setgid(user_gid)
+        os.setuid(user_uid)
+    return result
+
 def pushdir(dirname):
     global pushstack
     pushstack.append(os.getcwd())
-    od.chdir(dirname)
+    os.chdir(dirname)
 
 def popdir():
     global pushstack
@@ -119,7 +161,10 @@ def getlist(outdir, listfiles):
     pushdir(outdir)
     for i in listfiles:
         print ("Download " + i + " file")
-        getfile(i)
+        if not os.path.exists(os.path.basename(i)):
+            getfile(i)
+        else:
+            print ("File %s already exists." % os.path.basename(i))
     popdir()
 
 def get_timezone():
@@ -202,11 +247,33 @@ def mkswap(dev):
 
 def deltree(path):
     if path == "":
-        exit -1
+        exit(-1)
     cmd = []
     cmd.append("rm")
     cmd.append("-rvf")
     cmd.append(path)
+    p = sproc.Popen(cmd, stdin=sproc.PIPE, stdout=sproc.PIPE, stderr=sproc.STDOUT, shell=False)
+    line = p.stdout.readline()
+    while line != "":
+        sys.stdout.write(line)
+        sys.stdout.flush()
+        line = p.stdout.readline()
+    return p.wait()
+
+def run_with_env_as(user, cmds, env, cwd):
+    userRecord = pwd.getpwnam(user)
+    userUid = userRecord.pw_uid
+    userGid = userRecord.pw_gid
+    p = sproc.Popen(
+        cmds, stdin=sproc.PIPE, stdout=sproc.PIPE, stderr=sproc.STDOUT, preexec_fn=demote(userUid, userGid), cwd=cwd, env=env, shell=False)
+    line = p.stdout.readline()
+    while line != "":
+        sys.stdout.write(line)
+        sys.stdout.flush()
+        line = p.stdout.readline()
+    return p.wait()
+
+def run_cmd(cmd):
     p = sproc.Popen(cmd, stdin=sproc.PIPE, stdout=sproc.PIPE, stderr=sproc.STDOUT, shell=False)
     line = p.stdout.readline()
     while line != "":
@@ -222,22 +289,31 @@ def isblkdev(dev):
     else:
         return False
 
+def download_sources():
+    try:
+        os.makedirs('%s/sources' % os.environ['HOME'])
+    except:
+        print("Directory exists. Ignore.")
+    getlist('%s/sources' % os.environ['HOME'], WGET_LIST)
+
 #---------------------------stages-------------------------------------#
 def stage1():
+    global TIMEZONE
     TIMEZONE = get_timezone()
     deltree("/tools")
     deltree("/sources")
     deltree(LFS)
     f = open("build-properties", "a")
+    f.write("TIMEZONE=%s" % TIMEZONE)
     f.write("LFS=%s" % LFS)
     if os.path.isfile(ROOT_PART) and isblkdev(ROOT_PART):
         ret = mkfs(FS, ROOT_PART)
         if ret != 0:
             print "Error!"
-            exit(1)
+            exit(-1)
     else:
         print "%s is not a valid block device. Aborting..." % ROOT_PART
-        exit(1)
+        exit(-1)
     os.makedirs(LFS)
     mount(ROOT_PART, LFS)
     if os.path.isfile(SWAP_PART) and isblkdev(SWAP_PART):
@@ -246,7 +322,70 @@ def stage1():
             print "Swap partition exists and is active. Not formatting"
     else:
         print "No valid swap partition specified. Continuing without swap partition..."
+    if os.path.isfile(HOME_PART) and isblkdev(HOME_PART) and HOME_PART != ROOT_PART:
+        if FORMAT_HOME:
+            mkfs(FS, HOME_PART)
+        mount(HOME_PART, "%s/home" % LFS)
+    else:
+        print "No valid and different home partition specified. Continuing with home on root partition..."
+    os.makedirs("%s/sources" % LFS)
+    os.chmod("%s/sources" % LFS, 0o777)
+    os.symlink("%s/sources" % LFS, "/sources")
+    shutil.copytree("%s/sources/*" % os.environ['HOME'], "%s/sources" % LFS)
+    f = open("%s/sources/currentstage" % LFS, "w")
+    f.close()
+    os.makedirs("%s/tools" % LFS)
+    os.symlink("%s/tools" % LFS, "/")
+    os.makedirs("%s/var/cache/alps/sources" % LFS)
+    if os.path.isdir("%s/sources-apps" % os.environ['HOME']):
+        shutil.copytree("%s/sources-apps/*" % os.environ['HOME'], "%s/var/cache/alps/sources/" % LFS)
+        for root,dirs,files in os.walk("%s/var/cache/alps/sources" % LFS):
+            for name in files:
+                st = os.stat(name)
+                os.chmod(name, st.st_mode | stat.S_IWUSR | stat.S_IRUSR | stat.S_IWGRP | stat.S_IRGRP | stat.S_IWOTH | stat.S_IROTH)
+            for name in dirs:
+                st = os.stat(name)
+                os.chmod(name, st.st_mode | stat.S_IWUSR | stat.S_IRUSR | stat.S_IWGRP | stat.S_IRGRP | stat.S_IWOTH | stat.S_IROTH)
+    if 'lfs' in open('/etc/passwd').read():
+        run_cmd(['userdel', '-r', 'lfs'])
+    run_cmd(['groupadd', 'lfs'])
+    run_cmd(['useradd', '-s', '/bin/bash', '-g', 'lfs', '-m', '-k', '/dev/null', 'lfs'])
+    run_cmd(['chown', '-R', 'lfs:lfs', '/home/lfs/*'])
+    run_cmd(['chown', '-R', 'lfs:lfs', '/sources/*'])
+    run_cmd(['chown', '-R', 'lfs:lfs', '/home/lfs'])
+
+def stage2():
+    env = {}
+    PATH='/tools/bin:/bin:/usr/bin'
+    env['LFS'] = LFS
+    env['LC_ALL'] = LC_ALL
+    env['PATH'] = PATH
+    env['LFS_TGT']=LFS_TGT
+    for name in glob.glob('/sources/toolchain/*.sh'):
+        run_with_env_as('lfs', name, env, '/home/lfs')
+
+def stage3():
+    run_cmd(['chown', '-R', 'root:root', '%s/tools' % LFS])
+    for name in ['dev', 'proc', 'sys', 'run']:
+        os.makedirs("%s/%s" % (LFS, name), True)
+    if not os.path.exists("%s/dev/console" % LFS):
+        run_cmd(['mknod', '-m', '600', '%s/dev/console' % LFS, 'c', '5', '1'])
+    if not os.path.exists("%s/dev/null" % LFS):
+        run_cmd(['mknod', '-m', '600', '%s/dev/null' % LFS, 'c', '1', '3'])
+    run_cmd(['mount', '-v', '--bind', '/dev', '%s/dev' % LFS])
+    run_cmd(['mount', '-vt', 'devpts', 'devpts', '%s/dev/pts' % LFS, '-o', 'gid=5', 'mode=620'])
+    run_cmd(['mount', '-vt', 'proc', 'proc', '%s/proc' % LFS])
+    run_cmd(['mount', '-vt', 'sysfs', 'sysfs', '%s/sys' % LFS])
+    run_cmd(['mount', '-vt', 'tmpfs', 'tmpfs', '%s/run' % LFS])
+    if os.path.exists("%s/dev/shm" % LFS):
+        os.makedirs("%s/%s" % (LFS, os.readlink("%s/dev/shm" % LFS)))
+
+def stage4():
+    with Chroot(LFS):
+        run_cmd(['mkdir', '-pv', '/{bin,boot,etc/{opt,sysconfig},home,lib/firmware,mnt,opt}'])
+        run_cmd(['mkdir', '-pv', '/{media/{floppy,cdrom},sbin,srv,var}'])
     
+
 #---------------------------main---------------------------------------#
 
 if __name__ == "__main__":
@@ -269,3 +408,19 @@ if __name__ == "__main__":
 #print(ret)
 print isblkdev("/dev/sda1")
 print isblkdev("/home/marcellomortaro/src/git/aryalinux/lfs/lfs.py")
+#run_cmd(['mkdir', '-pv', '/home/marcellomortaro/chroot/bin'])
+#shutil.copy2('/bin/mkdir', '/home/marcellomortaro/chroot/bin')
+#shutil.copy2('/bin/bash', '/home/marcellomortaro/chroot/bin')
+
+#try:
+#    run_cmd(['mkdir', '-pv', '/home/marcellomortaro/chroot'])
+#    with Chroot('/home/marcellomortaro/chroot'):
+#        run_cmd(['mkdir', '-pv', '/{bin,boot,etc/{opt,sysconfig},home,lib/firmware,mnt,opt}'])
+#        print os.getcwd()
+#except:
+#    print "error"
+#os.chroot('/home/marcellomortaro/chroot')
+#run_cmd(['mkdir', '-pv', '/{bin,boot,etc/{opt,sysconfig},home,lib/firmware,mnt,opt}'])
+#print os.getcwd()
+download_sources()
+
